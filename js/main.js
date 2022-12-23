a66079cffacd9e131cd63e8686710351bc63ba71
Andy Learmouth <andymlearmouth@hotmail.com>
2022-12-23 07:53:04 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-//     .then(res => res.json()) // parse response as JSON
-//     .then(data => {
-//       console.log(data.drinks[0])
-//       document.querySelector('h2').innerText = data.drinks = data.drinks[0].strDrink
-//       document.querySelector('img').src = data.drinks
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     });
-
-
-
     document.querySelector('button').addEventListener('click', getDrink)
   
 
@@ -43,6 +29,7 @@
 
 
 document.querySelector('#button2').addEventListener('click', randomDrink)
+document.querySelector('#stop').addEventListener('click', stop)
 
     function randomDrink(){
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
